bf8d76652b66cd862d467b3abaf86560d9cc1752
Paddy-Nimrod <n.ambetsa21@gmail.com>
2024-12-08 15:16:01 +0300

    first commit

---
+++ backend/DoctorsLocumApp/src/main/java/com/softgroup4/DoctorsLocumApp/DoctorsLocumAppApplication.java
@@ -0,0 +1,13 @@
+package com.softgroup4.DoctorsLocumApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsLocumAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsLocumAppApplication.class, args);
+	}
+
+}
---
+++ backend/DoctorsLocumApp/src/test/java/com/softgroup4/DoctorsLocumApp/DoctorsLocumAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softgroup4.DoctorsLocumApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsLocumAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
